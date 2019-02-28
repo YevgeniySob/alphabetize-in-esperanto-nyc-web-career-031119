4ce1a1745337ed2a64c40eea97fc34efe68a15f9
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-28 21:56:49 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,4 +1,4 @@
 def alphabetize(arr)
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by {|phrase|  phrase}
+  arr.sort_by {|phrase|  phrase.split("").map {|char| ESPERANTO_ALPHABET.index(char)} }
 end
